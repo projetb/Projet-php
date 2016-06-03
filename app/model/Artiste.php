06349ecf6a8b79427afe4ff2ce1e23863328fffb
projetb <bxb77@hotmail.fr>
2016-06-03 15:03:51 +0200

    Creation class Artiste

---
+++ app/model/Artiste.php
@@ -0,0 +1,32 @@
+<?php
+class Artiste extends Model {
+	public $idArtiste, $pseudoArtiste, $description;
+	public static function setFromId( $id ,$data) {                                                                                                  
+		$db = Database::getInstance();
+		$sql = "UPDATE Artiste set id=:id,pseudoArtiste=:pseudoArtiste,description=:description WHERE id = :id";
+		$stmt = $db->prepare($sql);
+		//$stmt->setFetchMode(PDO::FETCH_CLASS, "Contact");
+		return $stmt->execute(array(
+			":id" => $id,
+			":pseudoArtiste"=>$data['pseudoArtiste'],
+			":description"=>$data['description']);
+		//return $stmt->fetch();
+	}
+	
+	public static function getFromId( $id ) {
+		$db = Database::getInstance();
+		$sql = "SELECT * FROM Artiste WHERE id = :id";
+		$stmt = $db->prepare($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Artiste");
+		$stmt->execute(array(":id" => $id));
+		return $stmt->fetch();
+	}
+	public static function getList() {
+		$db = Database::getInstance();
+		$sql = "SELECT * FROM Artiste";
+		$stmt = $db->query($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Artiste");
+		return $stmt->fetchAll();
+	}
+}
+?>
