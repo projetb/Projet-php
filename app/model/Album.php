5a1c548687f9bbdf6be07da7d797b10da1aa8205
projetb <bxb77@hotmail.fr>
2016-06-03 15:08:15 +0200

    Creation class Album

---
+++ app/model/Album.php
@@ -0,0 +1,33 @@
+<?php
+class Album extends Model {
+	public $idAlbum, $titre, $dateSortie, $genre, $noteGeneral;
+	public static function setFromId( $id,$data ) {                                                                                                  
+		$db = Database::getInstance();
+		$sql = "UPDATE Album set titre=:titre,dateSortie=:dateSortie,genre=:genre,noteGeneral=:noteGeneral WHERE id = :id";
+		$stmt = $db->prepare($sql);
+		//$stmt->setFetchMode(PDO::FETCH_CLASS, "Contact");
+		return $stmt->execute(array(
+			":id" => $id,
+			":titre"=>$data['titre'],
+			":dateSortie"=>$data['dateSortie'],
+			":genre"=>$data['genre'],
+      ":noteGeneral"=>$data['noteGeneral']));
+		//return $stmt->fetch();
+	}
+	public static function getFromId( $id ) {
+		$db = Database::getInstance();
+		$sql = "SELECT * FROM Album WHERE id = :id";
+		$stmt = $db->prepare($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Album");
+		$stmt->execute(array(":id" => $id));
+		return $stmt->fetch();
+	}
+	public static function getList() {
+		$db = Database::getInstance();
+		$sql = "SELECT * FROM Album";
+		$stmt = $db->query($sql);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, "Album");
+		return $stmt->fetchAll();
+	}
+}
+?>
