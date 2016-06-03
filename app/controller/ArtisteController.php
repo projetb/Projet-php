d8c76e034a225352c31921ae400a640d6c84930d
projetb <bxb77@hotmail.fr>
2016-06-03 15:31:51 +0200

    ajout du controller Artiste

---
+++ app/controller/ArtisteController.php
@@ -0,0 +1,14 @@
+<?php
+class ArtisteController extends Controller {
+	
+	public function afficherListe(){
+		$this->view->list = Artiste::getList();
+		$this->view->display(); 
+	}
+	public function afficherAlbum() {
+		$id = $this->route["params"]["id"];
+		$this->view->artiste = Artiste::getFromId($id);
+		$this->view->display();
+	}
+}
+?>
