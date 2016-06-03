5152f5d0a09d213bfeb81a402dd5e5d676767cb7
projetm <maximef91080gmail.com>
2016-06-03 15:46:53 +0200

    Inscription Controller

--- app/controller/InscriptionController.php
+++ app/controller/InscriptionController.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 class InscriptionController extends Controller {
    
@@ -5,3 +6,37 @@ class InscriptionController extends Controller {
       $this->view->display();
    }
 }
+=======
+<?php
+class InscriptionController extends Controller {
+   
+   public function Inscription() {
+      $this->view->display();
+   }
+  public function verifChamp(){
+     extract($_POST);
+      if(isset($login) && isset($pass) && isset($pass2) && isset($mail) && isset($fname) && isset($name)) {
+        if ($login=="" || $pass=="" || $pass2=="" || $mail=="" || $fname=="" || $name==""){
+               if ($pass!=$pass2){
+                   echo "<br>Mot de passe incorrect.";
+                }
+            echo "<br>Champs incomplets.Veuillez réessayer";
+        }
+        else if  ($login!="" || $pass!="" || $mail!="" || $fname!="" || $name!="") {
+             echo "<br>Inscription Réussi.";
+          $db = Database::getInstance();
+		     $sql = "INSERT INTO Utilisateur values(:login,:pass,:mail,:fname,:name)";
+		     $stmt = $db->prepare($sql);
+		  $stmt->setFetchMode(PDO::FETCH_CLASS, "Utilisateur");
+	  	$stmt->execute(array(":login" => $login,
+			":pass"=>$pass,
+			":mail"=>$mail,
+     	":fname"=>$fname,
+			":name"=>$name));
+	  	return $stmt->fetch();
+        }       
+      }
+  }
+}
+?>
+>>>>>>> Stashed changes
